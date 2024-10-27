ccefbed76bb4583a8870fc54dbfad867554e3e20
Serendipity <1076624311@qq.com>
2024-10-27 17:15:57 +0800

    upgrade

--- utils/dataPrePare.py
+++ utils/dataPrePare.py
@@ -1,68 +1,67 @@
 import os
 import re
-import random
-import shutil
-import cv2
-from tqdm import tqdm
 
-class DataPrepare:
-    def __init__(self, input_dir, output_dir):
-        self.input_dir = input_dir
-        self.output_dir = output_dir
+class DataPreparer:
+    def __init__(self):
+        pass
 
-    def replace_chinese_in_filenames(self, translation_dict):
+    def get_filenames_without_extension(self, folder_path):
         """
-        Replace Chinese characters in filenames with their English translations.
+        获取文件夹中所有文件的名称，不包含扩展名。
         """
-        # Check if the directory exists
-        if not os.path.isdir(self.input_dir):
-            print(f"The directory '{self.input_dir}' does not exist.")
-            return
-
-        # Iterate over files in the directory
-        for filename in os.listdir(self.input_dir):
-            # Check if the filename contains Chinese characters
-            if re.search(r'[\u4e00-\u9fff]+', filename):
-                # Replace Chinese characters with their English translations
-                new_filename = filename
-                for chinese, english in translation_dict.items():
-                    new_filename = new_filename.replace(chinese, english)
-
-                # If the filename has changed, rename the file
-                if new_filename != filename:
-                    old_file_path = os.path.join(self.input_dir, filename)
-                    new_file_path = os.path.join(self.input_dir, new_filename)
-                    os.rename(old_file_path, new_file_path)
-                    print(f"Renamed '{filename}' to '{new_filename}'")
+        filenames = os.listdir(folder_path)
+        filenames_without_extension = set(os.path.splitext(file)[0] for file in filenames)
+        return filenames_without_extension
 
     def compare_folders(self, folder_path_1, folder_path_2):
         """
-        Compare two folders and return the missing files.
+        比较两个文件夹中的文件，返回各自缺失的文件名。
         """
-        folder_1_files = set(os.path.splitext(file)[0] for file in os.listdir(folder_path_1))
-        folder_2_files = set(os.path.splitext(file)[0] for file in os.listdir(folder_path_2))
+        folder_1_files = self.get_filenames_without_extension(folder_path_1)
+        folder_2_files = self.get_filenames_without_extension(folder_path_2)
 
         missing_in_folder_1 = folder_2_files - folder_1_files
         missing_in_folder_2 = folder_1_files - folder_2_files
 
         return missing_in_folder_1, missing_in_folder_2
 
-    def image_data_enhancement(self, ratio=1.0):
+    def replace_chinese_in_filenames(self, directory_path, translation_dict):
         """
-        Apply image data enhancement techniques.
+        将文件名中的中文字符替换为英文翻译。
         """
-        # ... [Add methods for data enhancement like AddWeather, MirrorHorizon, etc.]
+        if not os.path.isdir(directory_path):
+            print(f"The directory '{directory_path}' does not exist.")
+            return
 
-    def __lable2txt(self, lableInfo, txtPath):
-        """
-        Convert label information to text file.
-        """
-        with open(txtPath, 'w') as f:
-            f.writelines([line + os.linesep for line in lableInfo])
+        for filename in os.listdir(directory_path):
+            if re.search(r'[\u4e00-\u9fff]+', filename):
+                new_filename = filename
+                for chinese, english in translation_dict.items():
+                    new_filename = new_filename.replace(chinese, english)
 
-    def save_file(self, image_path, label_path, file_name):
-        """
-        Save image and corresponding label file.
-        """
-        shutil.copy(image_path, self.output_dir + '/' + file_name)
-        self.__lable2txt(label_path, self.output_dir + '/' + file_name + '.txt')
+                if new_filename != filename:
+                    old_file_path = os.path.join(directory_path, filename)
+                    new_file_path = os.path.join(directory_path, new_filename)
+                    os.rename(old_file_path, new_file_path)
+                    print(f"Renamed '{filename}' to '{new_filename}'")
+
+# 使用示例
+if __name__ == "__main__":
+    preparer = DataPreparer()
+
+    # 比较文件夹
+    folder_path_1 = './data/xml'
+    folder_path_2 = './data/txt'
+    missing_in_1, missing_in_2 = preparer.compare_folders(folder_path_1, folder_path_2)
+    print(f"Missing in folder 1: {missing_in_1}")
+    print(f"Missing in folder 2: {missing_in_2}")
+
+    # # 替换文件名中的中文
+    # directory_path = './images'
+    # translation_dict = {
+    #     "砖石双坡屋顶": "brickHouse",
+    #     "彩钢瓦双坡厂房": "caiChangFang",
+    #     "测试": "test",
+    #     # 添加更多翻译
+    # }
+    # preparer.replace_chinese_in_filenames(directory_path, translation_dict)
