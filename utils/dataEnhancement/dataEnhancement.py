cc256de40acdf4e4564c6dd2ca68b598afac799f
Meng <1076624311@qq.com>
2024-12-03 00:01:49 +0800

    更新

--- utils/dataEnhancement/dataEnhancement.py
+++ utils/dataEnhancement/dataEnhancement.py
@@ -208,7 +208,7 @@ class aug:
             
             rotated_bgr = cv2.cvtColor(rotated, cv2.COLOR_RGB2BGR)
 
-            cv2.imwrite(self.ExportImagePath + '/' + name_only + flag + '.tif', rotated_bgr)
+            cv2.imwrite(self.ExportImagePath + '/' + name_only + flag + '.jpg', rotated_bgr)
             labelInfo = self.getBboxRotate(self.LabelPath + '/' + name_only + '.txt', angle, width, height, new_height / height)
             self.label2txt(labelInfo, self.ExportLabelPath + '/' + name_only + flag + '.txt')
 
@@ -340,20 +340,20 @@ class aug:
             transformed_pil = Image.fromarray(transformed['image'])
 
             # Save the transformed image as a TIF image
-            transformed_pil.save(self.ExportImagePath + '/' + name_only + flag + '.tif')
+            transformed_pil.save(self.ExportImagePath + '/' + name_only + flag + '.jpg')
 
             shutil.copy(self.LabelPath + '/' + name_only + '.txt', self.ExportLabelPath + '/' + name_only + flag + '.txt')
 
 
 if __name__ == "__main__":
-    aug = aug(r'D:\DAS_DATASET\data\test\datasets\train\images',
-              r'D:\DAS_DATASET\data\test\datasets\train\labels',
-              r'D:\DAS_DATASET\data\test\datasets\train\images\images_ex',
-              r'D:\DAS_DATASET\data\test\datasets\train\labels\labels_ex')
+    aug = aug(r'E:\test_data\test_data\images',
+              r'E:\test_data\test_data\labels',
+              r'E:\test_data\test_data\images',
+              r'E:\test_data\test_data\labels')
     
-    # aug.Rotate()
-    # aug.MirrorHV()
-    aug.AddWeather()
-    # aug.RandomCrop()
-    # aug.MirrorVertical()
-    # aug.MirrorHorizon()
+    aug.Rotate()
+    # # aug.MirrorHV()
+    # aug.AddWeather()
+    # # aug.RandomCrop()
+    # # aug.MirrorVertical()
+    # # aug.MirrorHorizon()
--- utils/vision.py
+++ utils/vision.py
@@ -20,43 +20,46 @@ def GetFileFromThisRootDir(dir, ext=None):
 def visualise_gt(label_path, pic_path, newpic_path):
     results = GetFileFromThisRootDir(label_path)
     for result in results:
+        name = os.path.splitext(os.path.basename(result))[0]
+        filepath = os.path.join(pic_path, f"{name}.jpg")
+        im = cv2.imread(filepath)
+        if im is None:
+            print(f"图片文件 {filepath} 读取失败")
+            continue
+        im_h, im_w = im.shape[:2]
+
         with open(result, 'r') as f:
             lines = f.readlines()
-        if not lines:  # 如果为空
+        if not lines:
             print('文件为空', result)
             continue
-        
-        # 过滤掉空行和包含少于8个元素的行
+
         boxes = []
         for line in lines:
-            parts = line.strip().split(' ')
-            parts = list(map(lambda x: float(x)*512, filter(None, parts)))
+            parts = line.strip().split()
             if len(parts) >= 8:
-                boxes.append(np.array(parts[1:9], dtype=np.float64))
+                coordinates = [float(x) for x in parts[1:9]]
+                for i in range(0, 8, 2):
+                    coordinates[i] *= im_w  # 修复方式：将i的起始值改为0，确保索引不越界
+                    coordinates[i+1] *= im_h
+                boxes.append(coordinates)
         if not boxes:
             print('没有有效的框数据', result)
             continue
-        
-        boxes = np.array(boxes)
-        
-        name = os.path.splitext(os.path.basename(result))[0]
-        filepath = os.path.join(pic_path, f"{name}.tif")
-        im = cv2.imread(filepath)
-        im_h = im.shape[0]
-        im_w = im.shape[1] 
 
-        
+        boxes = np.array(boxes, dtype=np.float16)
+
         for box in boxes:
-            box = np.array([[box[0], box[1]], [box[2], box[3]], [box[4], box[5]], [box[6], box[7]]], np.int32)
-            box = box.reshape((-1, 1, 2))
-            cv2.polylines(im, [box], True, (0, 0, 255), 2)
-        
+            box_coords = np.array([[box[0], box[1]], [box[2], box[3]], [box[4], box[5]], [box[6], box[7]]], np.int32)
+            box_coords = box_coords.reshape((-1, 1, 2))
+            cv2.polylines(im, [box_coords], True, (0, 0, 255), 2)
+
         cv2.imwrite(os.path.join(newpic_path, f"{name}.png"), im)
 
 if __name__ == '__main__':
-    pic_path = r"D:\DATA\dataset\images\train_ex"
-    label_path = r"D:\DATA\dataset\labels\train_ex"
-    newpic_path = 'vision/daylightingBand'
+    pic_path = r"E:\test_data\test_data\images"
+    label_path = r"E:\test_data\test_data\labels"
+    newpic_path = 'vision/test2'
     if not os.path.isdir(newpic_path):
         os.makedirs(newpic_path)
     visualise_gt(label_path, pic_path, newpic_path)
